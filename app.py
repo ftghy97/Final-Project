98e966ec6a76453844572fb39923f3387f298192
syaripahainun <73108899+syaripahainun@users.noreply.github.com>
2025-05-29 16:59:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -109,14 +109,7 @@ def run_ml_app():
     
     
     #If button is clilcked
-    if st.button("Predict Income"):
-        result = predict(age, workclass, final_weight, education, marital_status, occupation,
-            relationship, race, gender, capital_gain, capital_loss, hours_per_week, native_country)
 
-        if result == '>50k':
-            st.success(f'Result: Your predicted income is {result}')
-        else:
-            st.error(f'Result: Your predicted income is {result} ')
 
 def predict(age, workclass, final_weight, education, marital_status, occupation,
             relationship, race, gender, capital_gain, capital_loss, hours_per_week, native_country):
