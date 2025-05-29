042562ae16be8763e0b193bf22370894a66859e4
ftghy97 <firlimaratus@gmail.com>
2025-05-29 20:11:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,9 @@ def main():
         st.subheader("Home")
         st.markdown(desc_temp, unsafe_allow_html=True)
     elif choice == "Machine Learning App":
-        run_ml_app()
+        from ml_app 
+       import run_ml_app
+
 
 def run_ml_app():
     design = """<div style="padding:15px;">
