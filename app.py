0849797ec573c7c2c6a1506f2e15358db78650ed
syaripahainun <73108899+syaripahainun@users.noreply.github.com>
2025-05-29 18:46:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ import pandas as pd
 with open('xgboost_model.pkl', 'rb') as file:
    xgboost_model = pickle.load(file)
 
-with open('columns.pkl', 'rb') as f:
-    columns = pickle.load(f)
-
 html_temp = """<div style="background-color:#000;padding:10px;border-radius:10px">
                 <h1 style="color:#fff;text-align:center">Income Category Prediction</h1> 
                 <h4 style="color:#fff;text-align:center">Made for: Credit Team</h4> 
@@ -102,9 +99,9 @@ def run_ml_app():
 
 
     gender = left.selectbox('Gender', ('Male', 'Female'))
-    capital_gain = right.number_input('Capital_Gain')
-    capital_loss = left.number_input('Capital_loss')
-    hours_per_week = right.number_input('Hours_per_week')
+    capital_gain = right.text_input('Capital_Gain')
+    capital_loss = left.text_input('Capital_loss')
+    hours_per_week = right.text_input('Hours_per_week')
     native_country = left.selectbox('Native Country',('United-States','Cambodia','England','Puerto-Rico','Canada','Germany','Outlying-US(Guam-USVI-etc)',
                                     'India', 'Japan','Greece', 'South', 'China', 'Cuba', 'Iran', 'Honduras','Philippines', 'Italy','Poland','Jamaica', 'Vietnam', 
                                     'Mexico','Portugal', 'Ireland', 'France', 'Dominican-Republic', 'Laos','Ecuador','Taiwan', 'Haiti','Columbia', 'Hungary',
@@ -142,7 +139,7 @@ def predict(age, workclass, final_weight, education, marital_status, occupation,
     input_data[f'occupation_{occupation}'] = 1
     input_data[f'relationship_{relationship}'] = 1
     input_data[f'race_{race}'] = 1
-    input_data[f'gender_{gender}'] = 1
+    input_data[f'sex_{gender}'] = 1
     input_data[f'native_country_{native_country}'] = 1
 
     # Buat DataFrame
