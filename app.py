ab208871c0e11a50e20d7cbd51f091cc799c25c2
ftghy97 <firlimaratus@gmail.com>
2025-05-29 21:23:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,12 +26,12 @@ def main():
     menu = ["Machine Learning App"]
     choice = st.sidebar.selectbox("Menu", menu)
     
-     if choice == "Home":
+    if choice == "Home":
         st.subheader("Home")
         st.markdown(desc_temp, unsafe_allow_html=True)
     elif choice == "Machine Learning App":
         run_ml_app()
-       
+
 def run_ml_app():
     design = """<div style="padding:15px;">
                     <h1 style="color:#fff">Income Category Prediction</h1>
