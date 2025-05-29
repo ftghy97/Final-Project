a855f159c0f057a5bc1bd4ef47f165fa59462f56
Wisnu-Wicaksono <wisnuwicaksono753@gmail.com>
2025-05-29 19:48:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -77,41 +77,6 @@ def run_ml_app():
 def predict(age, workclass, final_weight, education, marital_status, occupation,
             relationship, race, gender, capital_gain, capital_loss, hours_per_week, native_country):
 
-    # Daftar semua kolom yang digunakan saat training
-    columns = [
-        'age', 'final_weight', 'capital_gain', 'capital_loss', 'hours_per_week',
-        'workclass_Private', 'workclass_State-gov', 'workclass_Self-emp-not-inc',
-        'education_Preschool', 'education_1st-4th', 'education_5th-6th', 'education_7th-8th',
-        'education_9th', 'education_10th', 'education_11th', 'education_12th', 'education_HS-grad',
-        'education_Some-college', 'education_Assoc-voc', 'education_Assoc-acdm', 'education_Bachelors',
-        'education_Masters', 'education_Prof-school', 'education_Doctorate',
-        'marital_status_Married-civ-spouse', 'marital_status_Divorced', 'marital_status_Never-married',
-        'marital_status_Separated', 'marital_status_Widowed', 'marital_status_Married-spouse-absent',
-        'occupation_Tech-support', 'occupation_Craft-repair', 'occupation_Other-service', 
-        'occupation_Sales', 'occupation_Exec-managerial', 'occupation_Prof-specialty',
-        'occupation_Handlers-cleaners', 'occupation_Machine-op-inspct', 'occupation_Adm-clerical',
-        'occupation_Farming-fishing', 'occupation_Transport-moving', 'occupation_Priv-house-serv',
-        'occupation_Protective-serv', 'occupation_Armed-Forces',
-        'relationship_Wife', 'relationship_Own-child', 'relationship_Husband', 'relationship_Not-in-family',
-        'relationship_Other-relative', 'relationship_Unmarried',
-        'race_White', 'race_Black', 'race_Asian-Pac-Islander', 'race_Amer-Indian-Eskimo',
-        'gender_Male', 'gender_Female',
-        'native_country_United-States', 'native_country_Cambodia', 'native_country_England', 
-        'native_country_Puerto-Rico', 'native_country_Canada', 'native_country_Germany', 
-        'native_country_Outlying-US(Guam-USVI-etc)', 'native_country_India', 'native_country_Japan',
-        'native_country_Greece', 'native_country_South', 'native_country_China', 'native_country_Cuba',
-        'native_country_Iran', 'native_country_Honduras', 'native_country_Philippines',
-        'native_country_Italy', 'native_country_Poland', 'native_country_Jamaica',
-        'native_country_Vietnam', 'native_country_Mexico', 'native_country_Portugal',
-        'native_country_Ireland', 'native_country_France', 'native_country_Dominican-Republic',
-        'native_country_Laos', 'native_country_Ecuador', 'native_country_Taiwan',
-        'native_country_Haiti', 'native_country_Columbia', 'native_country_Hungary',
-        'native_country_Guatemala', 'native_country_Nicaragua', 'native_country_Scotland',
-        'native_country_Thailand', 'native_country_Yugoslavia', 'native_country_El-Salvador',
-        'native_country_Trinadad&Tobago', 'native_country_Peru', 'native_country_Hong',
-        'native_country_Holand-Netherlands'
-    ]
-
     # Inisialisasi semua nilai 0
     input_data = {col: 0 for col in columns}
 
