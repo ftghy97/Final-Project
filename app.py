b8cc3f951b00abb35af806db5db272dab445315e
ftghy97 <firlimaratus@gmail.com>
2025-05-29 22:28:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -110,14 +110,14 @@ def run_ml_app():
                                     'Guatemala', 'Nicaragua', 'Scotland', 'Thailand', 'Yugoslavia', 'El-Salvador', 'Trinadad&Tobago', 'Peru', 'Hong','Holand-Netherlands'))
     
     
-     if st.button("Predict Income"):
-        result = predict(age, workclass, final_weight, education, marital_status,
-                         occupation, relationship, race, gender,
-                         capital_gain, capital_loss, hours_per_week, native_country)
-        if result == '>50K':
-            st.success("Income more than 50K")
-        else:
-            st.success("Income less or equal to 50K")
+   if st.button("Predict Income"):
+      result = predict(age, workclass, final_weight, education, marital_status,
+                        occupation, relationship, race, gender,
+                        capital_gain, capital_loss, hours_per_week, native_country)
+      if result == '>50K':
+         st.success("Income more than 50K")
+      else:
+         st.success("Income less or equal to 50K")
 
 
            
